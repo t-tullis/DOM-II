3d18549c190771fab1cc85cecca4a91319ceeaed
t-tullis <t.tullis34@gmail.com>
2019-01-29 14:32:43 -0800

    added some stopPropagation examples

--- js/index.js
+++ js/index.js
@@ -1,33 +1,55 @@
 // Your code goes here
-let bodyTag = document.querySelector('body')
-let logoHeading = document.querySelector('.logo-heading')
-let getAnchors = document.querySelectorAll('nav a')
-let getH2 = document.querySelectorAll('h2')
+let bodyTag = document.querySelector('body');
+let logoHeading = document.querySelector('.logo-heading');
+let getAnchors = document.querySelectorAll('nav a');
+let getH2 = document.querySelectorAll('h2');
 let randomColor = Math.floor(Math.random()*16777215).toString(16);
-let imageOpacity = document.querySelectorAll('img')
-let navHeader = document.querySelector('.main-navigation')
+let imageOpacity = document.querySelectorAll('img');
+let navHeader = document.querySelector('.main-navigation');
+let buttons = document.querySelectorAll('.btn');
+let bottomParagraphs = document.querySelectorAll('.destination p');
+let contentPick = document.querySelector('.content-pick');
+let mainNavigation = document.querySelector('.main-navigation');
+
 //console.log when page contents are loaded. Very useful on bigger sites
 window.addEventListener("load", e => {
     console.log("Page Loaded!");
   });
 
+//console.logs while dragging anything around
+  window.addEventListener('drag', e =>{
+      console.log(`You're Dragging ${e.target}`);
+  })
+
+  //alerts when you let go of the dragged item
+  window.addEventListener('dragend', e => {
+      alert('BOOM! You dropped it');
+  })
+
   //on resize opacity of images change
   window.addEventListener('resize', () =>{
       imageOpacity.forEach(image =>{
-          image.style.opacity = '0.5'
+          image.style.opacity = '0.5';
         })
-        navHeader.style.zIndex = '1'
+        navHeader.style.zIndex = '1';
   });
 
   //alerts whenever something is copied from page.
   document.addEventListener('copy', () =>{
-    alert(`You've copied something`)
+    alert(`You've copied something`);
 });
   
 //dbl click fun bus in nav
-  logoHeading.addEventListener('dblclick', () =>{
+  logoHeading.addEventListener('dblclick', (e) =>{
       bodyTag.style.color = 'teal';
-    //   console.log('Color Changed')
+      e.stopPropagation();
+      console.log('propagation stops "Fun Bus" from disappearing on the double-click');
+  })
+
+  //double click in the white-space of the nav and everything will disappear
+  mainNavigation.addEventListener('dblclick', (e) =>{
+      e.target.style.display = 'none';
+      console.log('Its Bubbling?!');
   })
 
 //Mouse Over
@@ -52,6 +74,21 @@ window.addEventListener('scroll', () =>{
     })
 })
 
-console.log(imageOpacity)
+
+//Use of stopPropagation
+buttons.forEach(button => {
+    button.addEventListener('click', e => {
+        e.target.style.color = 'purple';
+        e.stopPropagation();
+        console.log('fire a click event');
+        })
+    })
+
+contentPick.addEventListener('click', e =>{
+    console.log('Is it bubbling?');
+})
+
+
+
 
 
